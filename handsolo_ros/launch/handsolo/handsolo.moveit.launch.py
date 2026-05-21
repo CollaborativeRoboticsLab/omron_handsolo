7747eb4ffd7cd2f85223bebc6e68ce24c63b2a3e
Kalana Ratnayake <kalanaratnayake95@gmail.com>
2026-05-21 14:09:48 +0000

    updated launch files and added srdf

--- handsolo_ros/launch/handsolo/handsolo.moveit.launch.py
+++ handsolo_ros/launch/handsolo/handsolo.moveit.launch.py
@@ -1,99 +1,61 @@
 import os
-import sys
 import yaml
-import json
-import xacro
 from launch import LaunchDescription
-from launch.substitutions import Command
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
+from moveit_configs_utils import MoveItConfigsBuilder
 
-def load_file(package_name, file_path):
-    package_path = get_package_share_directory(package_name)
-    absolute_file_path = os.path.join(package_path, file_path)
-
-    try:
-        with open(absolute_file_path, 'r') as file:
-            return file.read()
-    except EnvironmentError: # parent of IOError, OSError *and* WindowsError where available
-        return None
 
 def load_yaml(package_name, file_path):
-    package_path = get_package_share_directory(package_name)
-    absolute_file_path = os.path.join(package_path, file_path)
+    absolute_file_path = os.path.join(get_package_share_directory(package_name), file_path)
 
     try:
         with open(absolute_file_path, 'r') as file:
             return yaml.safe_load(file)
-    except OSError:  # parent of IOError, OSError *and* WindowsError where available
+    except OSError:
         return None
-        
-def generate_launch_description():
-    # Configure robot_description
-    robot_description_config = xacro.process_file(
-        os.path.join(
-            get_package_share_directory('handsolo_description'),
-            'xacro',
-            'ld250_tm12x.urdf.xacro',
+
+
+def get_moveit_config():
+    moveit_config = (
+        MoveItConfigsBuilder('ld250_tm12x', package_name='tm12x_moveit_config')
+        .robot_description(
+            file_path=os.path.join(
+                get_package_share_directory('handsolo_description'),
+                'xacro',
+                'ld250_tm12x.urdf.xacro',
+            )
         )
+        .robot_description_semantic(
+            file_path=os.path.join(
+                get_package_share_directory('handsolo_ros'),
+                'config',
+                'ld250_tm12x.srdf',
+            )
+        )
+        .trajectory_execution(
+            file_path=os.path.join(
+                get_package_share_directory('tm12x_moveit_config'),
+                'config',
+                'moveit2_controllers.yaml',
+            )
+        )
+        .joint_limits(
+            file_path=os.path.join(
+                get_package_share_directory('tm12x_moveit_config'),
+                'config',
+                'joint_limits.yaml',
+            )
+        )
+        .planning_pipelines(default_planning_pipeline='ompl', pipelines=['ompl'])
+        .to_moveit_configs()
     )
-    robot_description = {'robot_description': robot_description_config.toxml()}
-
-    # SRDF Configuration
-    robot_description_semantic_config = load_file('tm12x_moveit_config'  , 'config/tm12x.srdf')
-    robot_description_semantic = {'robot_description_semantic': robot_description_semantic_config}
-
-    # Kinematics
-    kinematics_yaml = load_yaml('tm12x_moveit_config'  , 'config/kinematics.yaml')
-    robot_description_kinematics = {'robot_description_kinematics': kinematics_yaml}
+    moveit_config.planning_pipelines['ompl'] = load_yaml('tm12x_moveit_config', 'config/ompl_planning.yaml')
+    return moveit_config
 
 
-    # Planning Configuration
-    ompl_planning_pipeline_config = {
-        'planning_pipelines': ['ompl'],
-        'ompl': {
-            'planning_plugin': 'ompl_interface/OMPLPlanner',
-            'request_adapters': """default_planner_request_adapters/AddTimeOptimalParameterization 
-                                   default_planner_request_adapters/FixWorkspaceBounds 
-                                   default_planner_request_adapters/FixStartStateBounds 
-                                   default_planner_request_adapters/FixStartStateCollision 
-                                   default_planner_request_adapters/FixStartStatePathConstraints""",
-            'start_state_max_bounds_error': 0.1,
-        },
-    }
-
-    ompl_planning_yaml = load_yaml('tm12x_moveit_config'  , 'config/ompl_planning.yaml')
-    ompl_planning_pipeline_config['ompl'].update(ompl_planning_yaml)
-
-    # Trajectory Execution Configuration -> Controllers
-    controllers_yaml = load_yaml('tm12x_moveit_config', 'config/controllers.yaml')
-    moveit_controllers = {
-        'moveit_simple_controller_manager': controllers_yaml, 
-        'moveit_controller_manager': 'moveit_simple_controller_manager/MoveItSimpleControllerManager'
-    }
-
-    # Trajectory Execution Functionality
-    trajectory_execution = {
-        'moveit_manage_controllers': True,
-        'trajectory_execution.allowed_execution_duration_scaling': 1.2,
-        'trajectory_execution.allowed_goal_duration_margin': 0.5,
-        'trajectory_execution.allowed_start_tolerance': 0.1,
-    }
-
-    # Planning scene
-    planning_scene_monitor_parameters = {
-        'publish_planning_scene': True,
-        'publish_geometry_updates': True,
-        'publish_state_updates': True,
-        'publish_transforms_updates': True,
-    }
-
-    # Joint limits
-    joint_limits_yaml = {
-        'robot_description_planning': load_yaml(
-            'tm12x_moveit_config', 'config/joint_limits.yaml'
-        )
-    }
+def generate_launch_description():
+    moveit_config = get_moveit_config()
 
     # Start the actual move_group node/action server
     run_move_group_node = Node(
@@ -102,14 +64,7 @@ def generate_launch_description():
         output='screen',
         emulate_tty=True,
         parameters=[
-            robot_description,
-            robot_description_semantic,
-            robot_description_kinematics,           
-            ompl_planning_pipeline_config,
-            trajectory_execution,
-            moveit_controllers,
-            planning_scene_monitor_parameters,
-            joint_limits_yaml,
+            moveit_config.to_dict(),
             {"use_sim_time": True},
         ],
     )
--- handsolo_ros/launch/handsolo/handsolo.rviz.launch.py
+++ handsolo_ros/launch/handsolo/handsolo.rviz.launch.py
@@ -1,37 +1,61 @@
 import os
-import sys
 import yaml
-import json
-import xacro
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.conditions import IfCondition, UnlessCondition
-from launch.substitutions import Command, LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
-from launch.substitutions import PathJoinSubstitution
 from ament_index_python.packages import get_package_share_directory
-
-def load_file(package_name, file_path):
-    package_path = get_package_share_directory(package_name)
-    absolute_file_path = os.path.join(package_path, file_path)
-
-    try:
-        with open(absolute_file_path, 'r') as file:
-            return file.read()
-    except EnvironmentError: # parent of IOError, OSError *and* WindowsError where available
-        return None
+from moveit_configs_utils import MoveItConfigsBuilder
 
 
 def load_yaml(package_name, file_path):
-    package_path = get_package_share_directory(package_name)
-    absolute_file_path = os.path.join(package_path, file_path)
+    absolute_file_path = os.path.join(get_package_share_directory(package_name), file_path)
 
     try:
         with open(absolute_file_path, 'r') as file:
             return yaml.safe_load(file)
-    except OSError:  # parent of IOError, OSError *and* WindowsError where available
+    except OSError:
         return None
+
+
+def get_moveit_config():
+    moveit_config = (
+        MoveItConfigsBuilder('ld250_tm12x', package_name='tm12x_moveit_config')
+        .robot_description(
+            file_path=os.path.join(
+                get_package_share_directory('handsolo_description'),
+                'xacro',
+                'ld250_tm12x.urdf.xacro',
+            )
+        )
+        .robot_description_semantic(
+            file_path=os.path.join(
+                get_package_share_directory('handsolo_ros'),
+                'config',
+                'ld250_tm12x.srdf',
+            )
+        )
+        .trajectory_execution(
+            file_path=os.path.join(
+                get_package_share_directory('tm12x_moveit_config'),
+                'config',
+                'moveit2_controllers.yaml',
+            )
+        )
+        .joint_limits(
+            file_path=os.path.join(
+                get_package_share_directory('tm12x_moveit_config'),
+                'config',
+                'joint_limits.yaml',
+            )
+        )
+        .planning_pipelines(default_planning_pipeline='ompl', pipelines=['ompl'])
+        .to_moveit_configs()
+    )
+    moveit_config.planning_pipelines['ompl'] = load_yaml('tm12x_moveit_config', 'config/ompl_planning.yaml')
+    return moveit_config
 
 def generate_launch_description():
     use_moveit = LaunchConfiguration('use_moveit')
@@ -41,45 +65,11 @@ def generate_launch_description():
         default_value='true',
         description='Whether to start RViz with the MoveIt configuration'
     )
-
-    # Configure robot_description
-    robot_description_config = xacro.process_file(
-        os.path.join(
-            get_package_share_directory('handsolo_description'),
-            'xacro',
-            'ld250_tm12x.urdf.xacro',
-        )
-    )
-    robot_description = {'robot_description': robot_description_config.toxml()}
-
-    # SRDF Configuration
-    robot_description_semantic_config = load_file('tm12x_moveit_config'  , 'config/tm12x.srdf')
-    robot_description_semantic = {'robot_description_semantic': robot_description_semantic_config}
-
-    # Planning Configuration
-    ompl_planning_pipeline_config = {
-        'planning_pipelines': ['ompl'],
-        'ompl': {
-            'planning_plugin': 'ompl_interface/OMPLPlanner',
-            'request_adapters': """default_planner_request_adapters/AddTimeOptimalParameterization 
-                                   default_planner_request_adapters/FixWorkspaceBounds 
-                                   default_planner_request_adapters/FixStartStateBounds 
-                                   default_planner_request_adapters/FixStartStateCollision 
-                                   default_planner_request_adapters/FixStartStatePathConstraints""",
-            'start_state_max_bounds_error': 0.1,
-        },
-    }
-
-    # Kinematics
-    kinematics_yaml = load_yaml('tm12x_moveit_config'  , 'config/kinematics.yaml')
-    robot_description_kinematics = {'robot_description_kinematics': kinematics_yaml}
+    moveit_config = get_moveit_config()
 
     # RViz configurations
     moveit_rviz_config = PathJoinSubstitution([FindPackageShare('handsolo_ros'), 'rviz', 'handsolo-moveit.rviz'])
     nav2_rviz_cfg = PathJoinSubstitution([FindPackageShare('handsolo_ros'), 'rviz', 'handsolo-nav2.rviz'])
-
-    # Joint limits
-    joint_limits_yaml = {'robot_description_planning': load_yaml('tm12x_moveit_config', 'config/joint_limits.yaml')}
 
     # RViz
     moveit_rviz_node = Node(
@@ -91,11 +81,11 @@ def generate_launch_description():
         arguments=['-d', moveit_rviz_config],
         condition=IfCondition(use_moveit),
         parameters=[
-            robot_description,
-            robot_description_semantic,
-            ompl_planning_pipeline_config,
-            robot_description_kinematics,
-            joint_limits_yaml,
+            moveit_config.robot_description,
+            moveit_config.robot_description_semantic,
+            moveit_config.planning_pipelines,
+            moveit_config.robot_description_kinematics,
+            moveit_config.joint_limits,
         ],
     )
 
